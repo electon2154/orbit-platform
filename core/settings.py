19af145258a752432a94a8874afa9a4f9cfe5063
electon2154 <59528971+electon2154@users.noreply.github.com>
2025-05-01 16:21:47 -0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -218,7 +218,6 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "https://orbit-platform-production-e049.up.railway.app/",
 ]
 
 # Login/Logout URLs
